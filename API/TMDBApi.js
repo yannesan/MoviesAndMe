fe288a4d91bf02c3e5d73530e3305c9e971a0259
yannesan <yanne.marzouk@epitech.eu>
2020-08-20 16:38:56 +0200

    Now, the API works

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -3,6 +3,6 @@ const API_TOKEN = "e1400febfd0b838e0e3701e9645f7400";
 export function getFilmsFromApiWithSearchedText (text) {
 const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
 return fetch(url)
-.then((response)=> responses.json())
+.then((response)=> response.json())
 .catch((error)=> console.error(error))
 }
